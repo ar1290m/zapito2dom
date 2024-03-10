378ae8e24521e68ec550cb7bb979eca48fc14638
ar1290m <162921303+ar1290m@users.noreply.github.com>
2024-03-10 19:20:23 -0300

    Add files via upload

---
+++ precache-manifest.b8f887d9d02405404178b8261f1ebf15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898b5eed03f704ff94e217ba254bdd6d",
+    "url": "zapito2dom/index.html"
+  },
+  {
+    "revision": "9a5e22c65e79ceefbeb1",
+    "url": "zapito2dom/static/css/main.45cd1fa6.chunk.css"
+  },
+  {
+    "revision": "45f2377b46fcbedb5612",
+    "url": "zapito2dom/static/js/2.7269a688.chunk.js"
+  },
+  {
+    "revision": "f81f473e25a4eb2354d2",
+    "url": "zapito2dom/static/js/3.f0ac7bee.chunk.js"
+  },
+  {
+    "revision": "9a5e22c65e79ceefbeb1",
+    "url": "zapito2dom/static/js/main.206e0c0f.chunk.js"
+  },
+  {
+    "revision": "0eb93936025042817f89",
+    "url": "zapito2dom/static/js/runtime~main.ca0a8716.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdd3932b86fcf5b2fc5db52c97479402.js"
+  "zapito2dom/precache-manifest.b8f887d9d02405404178b8261f1ebf15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("zapito2dom/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
