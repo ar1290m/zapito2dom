f78f9038568d5b674cd35de0581da1f988e5041b
ar1290m <162921303+ar1290m@users.noreply.github.com>
2024-03-10 18:58:48 -0300

    start

---
+++ precache-manifest.bdd3932b86fcf5b2fc5db52c97479402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7538b6f6f33fffa7ee52731457b06604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a5e22c65e79ceefbeb1",
+    "url": "/static/css/main.45cd1fa6.chunk.css"
+  },
+  {
+    "revision": "45f2377b46fcbedb5612",
+    "url": "/static/js/2.7269a688.chunk.js"
+  },
+  {
+    "revision": "f81f473e25a4eb2354d2",
+    "url": "/static/js/3.f0ac7bee.chunk.js"
+  },
+  {
+    "revision": "9a5e22c65e79ceefbeb1",
+    "url": "/static/js/main.206e0c0f.chunk.js"
+  },
+  {
+    "revision": "92260d6f8b016a1d5493",
+    "url": "/static/js/runtime~main.87344103.js"
+  }
+]);
